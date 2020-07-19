bcbbbb25a296094ef9b765a6cc45dc2b21837372
NirCoren <nircoren@gmail.com>
2020-07-20 00:26:52 +0300

    Updates

---
+++ precache-manifest.aebc9b9e181b3b27c93948a61f38067d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab34f0f3c0ff4c7a9fe93b8eb2e90b5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5ee6b512f07be2b9bb4f",
+    "url": "/my-app/static/css/main.b6500b40.chunk.css"
+  },
+  {
+    "revision": "aad073d8e3de2e534ee3",
+    "url": "/my-app/static/js/2.86680e2b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-app/static/js/2.86680e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee6b512f07be2b9bb4f",
+    "url": "/my-app/static/js/main.53e1303a.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
